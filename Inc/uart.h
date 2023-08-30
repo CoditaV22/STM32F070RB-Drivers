42e79b2a44eda5bba37558e404affa367196ce88
CoditaV22 <133143695+CoditaV22@users.noreply.github.com>
2023-08-30 13:25:15 +0300

    Update uart.h

--- Inc/uart.h
+++ Inc/uart.h
@@ -7,7 +7,9 @@
 
 char uart2_read(void);
 void uart2_rxtx_init(void);
-void uart2_rxtx_interrupt_init(void);
+void uart2_rx_interrupt_init(void);
+
+#define ISR_RXNE		(1U << 5)
 
 
 #endif /* UART_H_ */
