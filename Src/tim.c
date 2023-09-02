a5cc85474ec3c7603ca7f393b1407a44b6b2b7fd
CoditaV22 <133143695+CoditaV22@users.noreply.github.com>
2023-09-02 11:09:41 +0300

    Update tim.c

--- Src/tim.c
+++ Src/tim.c
@@ -5,8 +5,31 @@
 #define OC_TOGGLE  ((1U << 4) | (1U << 5))
 #define CCER_CC1E  (1U << 0)
 #define GPIOAEN    (1U << 17)
+#define UIE 	   (1U << 0)
 
+void tim3_1Hz_interrupt(void)
+{
+	//Enable clock access to timer3
+	RCC -> APB1ENR |= TIM3EN;
 
+	//Set PSC Value
+	TIM3 -> PSC = 800 - 1;
+
+	//Set ARR Value
+	TIM3 -> ARR = 10000 - 1;
+
+	//Clear CNT
+	TIM3 -> CNT = 0;
+
+	//Enable the timer
+	TIM3 -> CR1 |= CEN;
+
+	/*Enable TIM Interrupt*/
+	TIM3 -> DIER |= UIE;
+
+	/*Enable TIM Interrupt in NVIC */
+	NVIC_EnableIRQ(TIM3_IRQn);
+}
 
 
 void tim3_1Hz_init(void)
@@ -59,25 +82,4 @@ void tim3_pa6_Output_Compare(void)
 
 	//Enable the timer
 	TIM3 -> CR1 |= CEN;
-}
-
-void tim3_pa6_input_capture(void)
-{
-	RCC -> AHBENR |= GPIOAEN;
-
-	GPIOA -> MODER &=! (1U << 12);
-	GPIOA -> MODER |= (1U << 13);
-
-	GPIOA -> AFR[0] |= (1U << 24);
-
-	RCC -> APB1ENR |= TIM3EN;
-
-	TIM3 -> PSC = 800 - 1;
-
-	TIM3 -> CCMR1 = (1U << 0);
-
-	TIM3 -> CCER |= (1U << 0);
-
-	TIM3 -> CR1 |= CEN;
-
 }
