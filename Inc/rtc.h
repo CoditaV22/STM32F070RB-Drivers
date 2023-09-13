cd2f5f93c47bb7fc01045d05d68baf11c79f04bb
CoditaV22 <133143695+CoditaV22@users.noreply.github.com>
2023-09-13 18:03:08 +0300

    Add files via upload

---
+++ Inc/rtc.h
@@ -0,0 +1,19 @@
+#ifndef __RTC_H__
+#define __RTC_H__
+#include <stdint.h>
+#include "stm32f0xx.h"
+
+
+void rtc_init(void);
+uint8_t rtc_convert_bcd2bin(uint8_t value);
+uint8_t rtc_convert_bin2bcd(uint8_t value);
+
+uint32_t rtc_date_get_day(void);
+uint32_t rtc_date_get_year(void);
+uint32_t rtc_date_get_month(void);
+
+uint32_t rtc_time_get_second(void);
+uint32_t rtc_time_get_minute(void);
+uint32_t rtc_time_get_hour(void);
+
+#endif
